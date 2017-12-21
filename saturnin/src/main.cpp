de0fb80c5c42aa63ab874525a39750dd4cf30a20
Runik <Runik@localhost>
2017-12-21 23:03:40 +0100

    Added libconfig

--- saturnin/src/main.cpp
+++ saturnin/src/main.cpp
@@ -17,15 +17,11 @@
 // limitations under the License.
 //
 
-#include <imgui.h>
-#include <imgui_impl_glfw_gl3.h>
+#pragma warning(disable:4275) // libconfig specific warning disable
+#include <libconfig.h++>
 #include <stdio.h>
-#include "video/opengl.h"
 #include "video/opengl_legacy.h"
 #include "video/opengl_modern.h"
-#include <GL/gl3w.h>
-#include <GLFW/glfw3.h>
-
 
 #include <boost/system/config.hpp>
 #include <boost/locale.hpp>
@@ -33,6 +29,7 @@
 using namespace std;
 using namespace boost::locale;
 using namespace saturnin::video;
+using namespace libconfig;
 
 static void error_callback(int error, const char* description)
 {
@@ -51,6 +48,21 @@ int main(int argc, char *argv[])
     // Display a message using current system locale
     cout << translate("Hello World") << endl;
 
+    Config cfg;
+    
+    try {
+        cfg.readFile("saturnin.cfg");
+    }
+    catch (const FileIOException &fioex) {
+        cout << translate("Could not read 'saturnin.cfg', file will be created");
+        cfg.clear();
+        Setting &root = cfg.getRoot();
+        root.add("video", Setting::TypeGroup);
+        Setting &video = root["video"];
+        video.add("legacy_opengl", Setting::TypeBoolean) = false;
+        cfg.writeFile("saturnin.cfg");
+    }
+
 
     bool isLegacy = false;
     if (isLegacy) {
@@ -59,137 +71,4 @@ int main(int argc, char *argv[])
     else {
         return runModernOpenGl();
     }
-    
-    // Setup window
-	glfwSetErrorCallback(error_callback);
-	
-	if (!glfwInit())
-		return 1;
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE);
-#if __APPLE__
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui OpenGL3 example", NULL, NULL);
-	
-    uint32_t major = glfwGetWindowAttrib(window, GLFW_CONTEXT_VERSION_MAJOR);
-    uint32_t minor = glfwGetWindowAttrib(window, GLFW_CONTEXT_VERSION_MINOR);
-    uint32_t revision = glfwGetWindowAttrib(window, GLFW_CONTEXT_REVISION);
-    cout << "Context version :" << major << "." <<minor << " Revision: " << revision << endl;
-    glfwMakeContextCurrent(window);
-	glfwSwapInterval(1); // Enable vsync
-	gl3wInit();
-
-	// Setup ImGui binding
-	ImGui_ImplGlfwGL3_Init(window, true);
-
-	// Load Fonts
-	// (there is a default font, this is only if you want to change it. see extra_fonts/README.txt for more details)
-	//ImGuiIO& io = ImGui::GetIO();
-	//io.Fonts->AddFontDefault();
-	//io.Fonts->AddFontFromFileTTF("../../extra_fonts/Cousine-Regular.ttf", 15.0f);
-	//io.Fonts->AddFontFromFileTTF("../../extra_fonts/DroidSans.ttf", 16.0f);
-	//io.Fonts->AddFontFromFileTTF("../../extra_fonts/Roboto-Medium.ttf", 16.0f);
-	//io.Fonts->AddFontFromFileTTF("../../extra_fonts/ProggyTiny.ttf", 10.0f);
-	//io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-
-	bool show_test_window = true;
-	bool show_another_window = false;
-	bool show_video = true;
-	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
-    //OpenGl opengl;
-    //uint32_t fbo = opengl.createFramebuffer();
-    //opengl.setupTriangle();
-
-
-	// Main loop
-	while (!glfwWindowShouldClose(window))
-	{
-
-		glfwPollEvents();
-		ImGui_ImplGlfwGL3_NewFrame();
-
-		// 1. Show a simple window
-		// Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
-		{
-			static float f = 0.0f;
-			ImGui::Text("Hello, world!");
-			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
-			ImGui::ColorEdit3("clear color", (float*)&clear_color);
-			if (ImGui::Button("Test Window")) show_test_window ^= 1;
-			if (ImGui::Button("Another Window")) show_another_window ^= 1;
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		}
-
-
-		// 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
-		if (show_test_window)
-		{
-			ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver);
-			ImGui::ShowTestWindow(&show_test_window);
-		}
-
-        
-  //      if (show_video) {
-		//	ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Once);
-		//	ImGui::SetNextWindowSize(ImVec2(320, 200 + 20)); // + 20 
-
-  //          ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-  //          ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-  //          
-		//	ImGui::Begin("Video rendering", &show_video);
-		//	
-
-  //          glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-  //          glViewport(0, 0, 320, 200);
-  //          glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-  //          glClear(GL_COLOR_BUFFER_BIT);
-
-  //          //opengl.setupTriangle();
-  //          opengl.drawTriangle();
-
-  //          GLint ret;
-  //          glGetFramebufferAttachmentParameteriv(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_FRAMEBUFFER_ATTACHMENT_OBJECT_TYPE, &ret);
-  //          if (ret != GL_NONE) {
-  //              int32_t texture = 0;
-  //              glGetFramebufferAttachmentParameteriv(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_FRAMEBUFFER_ATTACHMENT_OBJECT_NAME, &texture);
-
-  //              ImGui::GetWindowDrawList()->AddImage(
-  //                  (void *)texture,
-  //                  ImVec2(ImGui::GetCursorScreenPos().x, ImGui::GetCursorScreenPos().y),
-  //                  ImVec2(ImGui::GetCursorScreenPos().x + 320, ImGui::GetCursorScreenPos().y + 200),
-  //                  ImVec2(0, 1), ImVec2(1, 0));
-
-  //          }
-  //          glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-
-
-  //          //ImGui::Text("Hello from another window!");
-		//	
-		//	ImGui::End();
-  //          ImGui::PopStyleVar();
-  //          ImGui::PopStyleVar();
-		//}
-
-
-		// Rendering
-		int display_w, display_h;
-		glfwGetFramebufferSize(window, &display_w, &display_h);
-		glViewport(0, 0, display_w, display_h);
-		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		ImGui::Render();
-        glfwSwapBuffers(window);
-	}
-
-	// Cleanup
-	ImGui_ImplGlfwGL3_Shutdown();
-	glfwTerminate();
-
-	return 0;
 }
