3c2834e9051fb8302ba8f7c9df3ecb115393ad7d
Runik <Runik@localhost>
2018-02-04 22:59:59 +0100

    Log plugin update

--- saturnin/src/log.h
+++ saturnin/src/log.h
@@ -18,12 +18,65 @@
 
 #pragma once
 
-#pragma warning(disable:4275) // libconfig specific warning disable
-#include <libconfig.h++>
+#include <memory>
+#include <string>
+#include <spdlog/spdlog.h>
 
 namespace saturnin {
 namespace core {
+    
+    class Log {
+    public:
+        enum class Type {
+            VDP1,
+            VDP2,
+            OPENGL,
+            CDROM,
+            SMPC,
+            M68K,
+            SCSP,
+            SOUND,
+            SH2,
+            SH2_MASTER,
+            SH2_SLAVE
+        };
 
+        enum class Severity {
+            DEBUG,
+            INFO,
+            WARNING,
+            ERR,
+            FATAL
+        };
+
+        //@{
+        // Constructors / Destructors
+        Log();
+        Log(const Log&)              = delete;
+        Log(Log&&)                   = delete;
+        Log& operator=(const Log&) & = delete;
+        Log& operator=(Log&&) &      = delete;
+        ~Log();
+        //@}
+
+        void log(const std::string& value) const;
+
+        std::shared_ptr<spdlog::sinks::simple_file_sink_mt> createSink(const std::string& logger_path);
+
+        std::shared_ptr<spdlog::logger> createLogger(const std::string& logger_name, const std::string& logger_path);
+        std::shared_ptr<spdlog::logger> createLogger(const std::string& logger_name, const std::shared_ptr<spdlog::sinks::simple_file_sink_mt>& sink);
+        std::shared_ptr<spdlog::logger> createLogger(const std::string& logger_name, const std::vector<spdlog::sink_ptr>& sinks);
+        std::shared_ptr<spdlog::logger> createLogger(const std::string& logger_name, const std::string& logger_path, const std::shared_ptr<spdlog::sinks::simple_file_sink_mt>& globalSink);
+
+        void createOrEmptyDirectory(const std::string& path);
+
+    private:
+        std::string pattern_;
+        std::shared_ptr<spdlog::sinks::simple_file_sink_mt> globalSink_;
+        std::shared_ptr<spdlog::logger> global_;
+        std::shared_ptr<spdlog::logger> vdp1_;
+        std::shared_ptr<spdlog::logger> cdrom_;
+    };
 
 };
 };
--- saturnin/src/main.cpp
+++ saturnin/src/main.cpp
@@ -18,19 +18,14 @@
 //
 
 #include <iostream> // cout
-#include <boost/filesystem.hpp>
-#include <boost/dll/import.hpp> // for import_alias
-#include "../../dll_log/src/log_plugin.h"
-//#include <spdlog/sinks/file_sinks.h>
 #include "locale.h"
 #include "config.h"
+#include "log.h"
 #include "video/opengl.h"
 #include "video/opengl_legacy.h"
 #include "video/opengl_modern.h"
 
-namespace dll = boost::dll;
 using namespace std;
-using namespace saturnin::log;
 using namespace saturnin::video;
 using namespace saturnin::core;
 
@@ -44,33 +39,38 @@ int main(int argc, char *argv[])
     if ( !Locale::initialize() ) return 1;
     cout << saturnin::core::tr("Hello World") << endl;
     
+    Log log;
+    log.log("test");
 
-    
-    boost::filesystem::path lib_path(boost::filesystem::current_path());          // argv[1] contains path to directory with our plugin library
-    boost::shared_ptr<LogPlugin> plugin;            // variable to hold a pointer to plugin variable
-    std::cout << "loading the plugin" << std::endl;
-    try {
-        plugin = dll::import<LogPlugin>(    // type of imported symbol is located between `<` and `>`
-            lib_path / "log",                // path to the library and library name
-            "plugin",                           // name of the symbol to import
-            dll::load_mode::append_decorations  // makes `libmy_plugin_sum.so` or `my_plugin_sum.dll` from `my_plugin_sum`
-            );
-        std::cout << "Version:  " << plugin->version() << std::endl;
-        plugin->log("test");
+    spdlog::get("global")->debug("global test");
+    spdlog::get("cdrom")->error("cdrom test");
+    spdlog::get("vdp1")->warn("vdp1 test");
 
-    //    auto globalSink = plugin->createSink("logs/global.log");
-    //    auto global = plugin->createLogger("global", globalSink);
-    //    auto cdrom = plugin->createLogger("cdrom", "logs/cdrom.log", globalSink);
-    //    auto vdp1 = plugin->createLogger("vdp1", "logs/vdp1.log", globalSink);
+    //boost::filesystem::path lib_path(boost::filesystem::current_path());          // argv[1] contains path to directory with our plugin library
+    //boost::shared_ptr<LogPlugin> plugin;            // variable to hold a pointer to plugin variable
+    //std::cout << "loading the plugin" << std::endl;
+    //try {
+    //    plugin = dll::import<LogPlugin>(    // type of imported symbol is located between `<` and `>`
+    //        lib_path / "log",                // path to the library and library name
+    //        "plugin",                           // name of the symbol to import
+    //        dll::load_mode::append_decorations  // makes `libmy_plugin_sum.so` or `my_plugin_sum.dll` from `my_plugin_sum`
+    //        );
+    //    std::cout << "Version:  " << plugin->version() << std::endl;
+    //    plugin->log("test");
 
-    //    global->info("test global");
-    //    cdrom->info("cdrom test");
-    //    vdp1->info("vdp1 test");
+    ////    auto globalSink = plugin->createSink("logs/global.log");
+    ////    auto global = plugin->createLogger("global", globalSink);
+    ////    auto cdrom = plugin->createLogger("cdrom", "logs/cdrom.log", globalSink);
+    ////    auto vdp1 = plugin->createLogger("vdp1", "logs/vdp1.log", globalSink);
 
-    }
-    catch (const boost::system::system_error& e) {
-        std::cout << e.what() << std::endl;
-    }
+    ////    global->info("test global");
+    ////    cdrom->info("cdrom test");
+    ////    vdp1->info("vdp1 test");
+
+    //}
+    //catch (const boost::system::system_error& e) {
+    //    std::cout << e.what() << std::endl;
+    //}
 
 
 
@@ -81,4 +81,4 @@ int main(int argc, char *argv[])
     bool isLegacyOpenGl = cfg.lookup("rendering.legacy_opengl");
 
     if (isLegacyOpenGl) return runLegacyOpenGl(); else return runModernOpenGl();
-}
+};
