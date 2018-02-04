43eb3b302354e8b4462d5839dcc63de57ac5ce07
Runik <34705093+rtoumazet@users.noreply.github.com>
2018-02-04 22:59:59 +0100

    Log plugin update

--- saturnin/src/log.cpp
+++ saturnin/src/log.cpp
@@ -17,34 +17,83 @@
 // limitations under the License.
 //
 
-//#include <iostream>                // cout
-//#include <boost/system/config.hpp> // needed by boost/locale
-//#include <boost/locale.hpp>        // generator
-//#include "locale.h"
-//
-//using namespace boost::locale;
-//using namespace std;
-//
-//namespace saturnin {
-//namespace core {
-//
-//    /* static */
-//    bool Locale::initialize()
-//    {
-//        generator gen;
-//        // Specify location of dictionaries
-//        gen.add_messages_path(".");
-//        gen.add_messages_domain("saturnin");
-//        // Generate locales and imbue them to iostream
-//        locale::global(gen(""));
-//        cout.imbue(locale());
-//
-//        return true;
-//    }
-//
-//    string tr(const string& str) {
-//        return translate(str);
-//    }
-//
-//};
-//};
+#include <iostream>
+#include <boost/filesystem.hpp>
+#include "log.h"
+
+using namespace std;
+
+namespace saturnin {
+namespace core {
+
+    Log::Log() {
+        std::cout << "Constructing my_plugin_log" << std::endl;
+        pattern_ = "[%R] [%n] %v";
+
+        auto sink = std::make_shared<spdlog::sinks::simple_file_sink_mt>("logs/global.log");
+            
+        auto global = createLogger("global", sink);
+        auto cdrom = createLogger("cdrom", "logs/cdrom.log", sink);
+        auto vdp1 = createLogger("vdp1", "logs/vdp1.log", sink);
+    }
+
+    void Log::log(const std::string& value) const {
+        auto console = spdlog::stdout_logger_mt("console");
+        console->info(value);
+    }
+
+    std::shared_ptr<spdlog::sinks::simple_file_sink_mt> Log::createSink(const std::string & logger_path)
+    {
+        createOrEmptyDirectory(logger_path);
+
+        return std::make_shared<spdlog::sinks::simple_file_sink_mt>(logger_path);
+    }
+
+    std::shared_ptr<spdlog::logger> Log::createLogger(const std::string& logger_name, const std::string& logger_path) {
+        createOrEmptyDirectory(logger_path);
+        auto full_path = boost::filesystem::current_path() / logger_path;
+        auto logger = spdlog::basic_logger_mt(logger_name, full_path.string());
+        logger.get()->set_pattern(pattern_);
+        return logger;
+    }
+
+    std::shared_ptr<spdlog::logger> Log::createLogger(const std::string & logger_name, const std::shared_ptr<spdlog::sinks::simple_file_sink_mt>& sink)
+    {
+        auto logger = std::make_shared<spdlog::logger>(logger_name, sink);
+        logger.get()->set_pattern(pattern_);
+        return logger;
+    }
+
+    std::shared_ptr<spdlog::logger> Log::createLogger(const std::string & logger_name, const std::vector<spdlog::sink_ptr>& sinks)
+    {
+        auto logger = std::make_shared<spdlog::logger>(logger_name, begin(sinks), end(sinks));
+        logger.get()->set_pattern(pattern_);
+        return logger;
+    }
+
+    std::shared_ptr<spdlog::logger> Log::createLogger(const std::string & logger_name, const std::string & logger_path, const std::shared_ptr<spdlog::sinks::simple_file_sink_mt>& globalSink)
+    {
+
+        createOrEmptyDirectory(logger_path);
+        std::vector<spdlog::sink_ptr> sinks;
+        sinks.push_back(globalSink);
+        sinks.push_back(std::make_shared<spdlog::sinks::simple_file_sink_mt>(logger_path));
+
+        auto logger = std::make_shared<spdlog::logger>(logger_name, begin(sinks), end(sinks));
+        logger.get()->set_pattern(pattern_);
+        return logger;
+    }
+
+    void Log::createOrEmptyDirectory(const std::string& path) {
+        auto full_path = boost::filesystem::current_path() / path;
+        full_path.make_preferred();
+        boost::filesystem::create_directories(full_path.parent_path());
+        boost::filesystem::remove(full_path);
+    }
+
+    Log::~Log() {
+        std::cout << "Destructing my_plugin_log ;o)" << std::endl;
+    }
+
+}
+}
