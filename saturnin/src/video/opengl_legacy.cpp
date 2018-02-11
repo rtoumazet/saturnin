21485141eecf35d11cf90add98e4bb54f1c0b05f
Runik <34705093+rtoumazet@users.noreply.github.com>
2018-02-11 22:48:44 +0100

    Started working on GUI

--- saturnin/src/video/opengl_legacy.cpp
+++ saturnin/src/video/opengl_legacy.cpp
@@ -21,13 +21,15 @@
 #include <iostream> // cout
 #include <epoxy/gl.h>
 #include <epoxy/wgl.h>
-#include "imgui/imgui.h"
+//#include "imgui/imgui.h"
+#include "gui.h"
 #include "bindings/imgui_impl_glfw.h"
 #include <GLFW/glfw3.h>
 #include "opengl_legacy.h"
 #include "opengl.h"
 
 using namespace std;
+using namespace saturnin;
 
 //static HPBUFFERARB pBuffer;
 //static HDC         pBufferHDC, screenHDC;
@@ -92,7 +94,7 @@ namespace video {
         bool show_test_window    = true;
         bool show_another_window = false;
         bool show_video          = true;
-        ImVec4 clear_color       = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
         // Main loop
         while (!glfwWindowShouldClose(window))
@@ -100,34 +102,15 @@ namespace video {
             glfwPollEvents();
             ImGui_ImplGlfwGL2_NewFrame();
 
-            // 1. Show a simple window
-            // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
-            {
-                static float f = 0.0f;
-                ImGui::Text("Hello, world!");
-                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
-                ImGui::ColorEdit3("clear color", (float*)&clear_color);
-                if (ImGui::Button("Test Window")) show_test_window ^= 1;
-                if (ImGui::Button("Another Window")) show_another_window ^= 1;
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            }
+            gui::showCoreWindow();
 
-            // 2. Show another simple window, this time using an explicit Begin/End pair
-            if (show_another_window)
-            {
-                ImGui::Begin("Another Window", &show_another_window);
-                ImGui::Text("Hello from another window!");
-                ImGui::End();
-            }
+            gui::showSimpleWindow(show_test_window, show_another_window);
 
-            // 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
-            if (show_test_window)
-            {
-                ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver);
-                ImGui::ShowTestWindow();
-            }
+            gui::showAnotherWindow(show_another_window);
 
-            if (show_video) {
+            gui::showTestWindow(show_test_window);
+
+            {
                 ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Once);
                 ImGui::SetNextWindowSize(ImVec2(320, 200 + 20)); // + 20 
 
