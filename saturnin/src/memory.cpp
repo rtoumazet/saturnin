69e7f48f9232db70923c3bff29ec5a13f112ab17
Runik <Runik@localhost>
2018-06-14 22:56:23 +0200

    WIP: migrating data to memory.cpp

---
+++ saturnin/src/memory.cpp
@@ -0,0 +1,149 @@
+// 
+// memory.cpp
+// Saturnin
+//
+// Copyright (c) 2018 Renaud Toumazet
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+
+#include "memory.h"
+
+namespace saturnin {
+namespace core {
+
+    bool Memory::StvLoadRomToMemory(const string &		zipname,
+        const string &		romname,
+        uint8_t *			destination,
+        const uint32_t &	size,
+        const Rom_load rom_load,
+        const uint8_t  times_mirrored,
+        const Rom_type rom_type)
+    {
+
+        HZIP zipFileHandle;
+
+        zipFileHandle = OpenZip((void*)zipname.c_str(), 0, ZIP_FILENAME);
+        if (!zipFileHandle) {
+            std::cout << "Error opening zip file" << endl;
+        }
+        else {
+            std::cout << "Zip file opened" << endl;
+
+            ZIPENTRY ze;
+            //int32_t index = 0;
+            int index = 0;
+            if (FindZipItem(zipFileHandle, romname.c_str(), true, &index, &ze) != ZR_OK) {
+                MessageBox(NULL, "Couldn't find the file in the zip", NULL, MB_OK);
+            }
+            else {
+                if (rom_type == Rom_type::bios) {
+                    //int8_t* content = new int8_t[size];
+                    //UnzipItem(zipFileHandle, index, (void *)content, size, ZIP_MEMORY);
+
+                    //uint32_t counter = size / 4;
+                    //// filling the rom data
+                    //// Needs byteswapping
+                    //for (uint32_t i = 0; i < counter; i++) {
+                    //    destination[i * 4 + 1] = content[i * 4 + 0];
+                    //    destination[i * 4 + 0] = content[i * 4 + 1];
+                    //    destination[i * 4 + 3] = content[i * 4 + 2];
+                    //    destination[i * 4 + 2] = content[i * 4 + 3];
+                    //}
+
+                    //delete[] content;
+
+                }
+                else {
+
+                    switch (rom_load) {
+                    case Rom_load::not_interleaved:
+                        //// Data is loaded normally
+                        //UnzipItem(zipFileHandle, index, (void *)destination, size, ZIP_MEMORY);
+
+                        //if (romType == PROGRAM) {
+                        //    // bios region is forced for program roms
+                        //    pCart[0x40] = pRom[0x808];
+                        //}
+                        //// ROM was loaded, checking if it needs to be mirrored
+                        //if (mirroringNumber != 0) {
+                        //    // ROM has to be mirrored
+                        //    for (uint8_t i = 1; i <= mirroringNumber; i++) {
+                        //        std::copy(destination, destination + size - 1, destination + (i*size));
+                        //    }
+                        //}
+                        break;
+                    case Rom_load::odd_interleaved:
+                    {
+                        //// Data is loaded on odd bytes only
+                        //int8_t* content = new int8_t[size * 2];
+                        //UnzipItem(zipFileHandle, index, (void *)content, size * 2, ZIP_MEMORY);
+
+                        //for (uint32_t i = 0; i < size; i++)
+                        //{
+                        //    destination[(i * 2 + 1)] = content[i];
+                        //}
+                        //delete[] content;
+
+                        //if (romType == PROGRAM) {
+                        //    // bios region is forced for program roms
+                        //    //pCart[0x80/4].val8[1] = pRom[0x808];
+                        //    pCart[0x81] = pRom[0x808];
+                        //}
+
+                        //// ROM was loaded, checking if it needs to be mirrored
+                        //if (mirroringNumber != 0) {
+                        //    // ROM has to be mirrored
+                        //    for (uint8_t i = 1; i <= mirroringNumber; i++) {
+                        //        std::copy(destination, destination + size * 2 - 1, destination + (i*size * 2));
+                        //    }
+                        //}
+                    }
+                    break;
+                    case Rom_load::even_interleaved:
+                    {
+                        //// Data is loaded on even bytes only
+                        //int8_t* content = new int8_t[size];
+                        //UnzipItem(zipFileHandle, index, (void *)content, size, ZIP_MEMORY);
+
+                        //for (uint32_t i = 0; i < size; i++)
+                        //{
+                        //    destination[i * 2] = content[i];
+                        //}
+                        //delete[] content;
+
+                        //if (romType == PROGRAM) {
+                        //    // bios region is forced for program roms
+                        //    //pCart[0x80/4].val8[1] = pRom[0x808];
+                        //    pCart[0x81] = pRom[0x808];
+                        //}
+
+                        //// ROM was loaded, checking if it needs to be mirrored
+                        //if (mirroringNumber != 0) {
+                        //    // ROM has to be mirrored
+                        //    for (uint8_t i = 1; i <= mirroringNumber; i++) {
+                        //        std::copy(destination, destination + size * 2 - 1, destination + (i*size * 2));
+                        //    }
+                        //}
+                    }
+                    break;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+}
+}
