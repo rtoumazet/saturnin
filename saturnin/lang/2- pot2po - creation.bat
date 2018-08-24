441658be206674e77780bb2cb37c73fc33b97096
Runik <34705093+rtoumazet@users.noreply.github.com>
2018-08-24 14:27:32 +0200

    WIP on locales, added automatic pot/po/mo generation using a batch file

--- saturnin/lang/2- pot2po - creation.bat
+++
@@ -1,3 +0,0 @@
-@echo on
-%xgettext_path%\msginit.exe --input=po/saturnin.pot --locale=fr --output=po/fr/saturnin.po
-
--- saturnin/lang/2- pot2po - update.bat
+++
@@ -1,2 +0,0 @@
-@echo on
-%xgettext_path%\msgmerge.exe -U po/fr/saturnin.po po/saturnin.pot
