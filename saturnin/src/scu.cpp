030e921fcbbe4d19b4c4c088a2f7c2a0f255672c
Runik <34705093+rtoumazet@users.noreply.github.com>
2019-01-22 20:11:48 +0100

    Added scu logger. Added scu pointer to memory object. Binded 32bits access for scu memory.

--- saturnin/src/scu.cpp
+++ saturnin/src/scu.cpp
@@ -18,6 +18,7 @@
 // 
 
 #include "scu.h"
+#include "memory.h"
 #include "utilities.h" // toUnderlying
 
 namespace util = saturnin::utilities;
@@ -29,19 +30,97 @@ Scu::Scu(Memory* m) : memory_(m) {
     initializeRegisters();
 };
 
+u32 Scu::read32(const u32 addr) const {
+    switch (addr) {
+        case dsp_program_control_port:
+            Log::debug("scu", "DSP - Program Control Port read");
+            //data = current_DSP_program_control_port_;
+            break;
+        case dsp_program_ram_dataport:
+            Log::debug("scu", "DSP - Program Dataport read");
+            //data = current_DSP_program_dataport_;
+            break;
+        case dsp_data_ram_address_port:
+            Log::debug("scu", "DSP - Data RAM Address Port read");
+            //data = current_DSP_data_ram_address_port_;
+            break;
+        case dsp_data_ram_data_port:
+            Log::debug("scu", "DSP - Data RAM Dataport read");
+            //data = current_DSP_data_ram_dataport_;
+            break;
+        default:
+            return rawRead<u32>(memory_->scu_, addr & 0xFF);
+    }
+    
+    return rawRead<u32>(memory_->scu_, addr & 0xFF);
+
+};
+
+void Scu::write32(const u32 addr, const u32 data) {
+//    uint8_t index = 0;
+//    uint32_t temp = 0;
+//
+//    switch (addr) {
+//        case dsp_program_control_port:
+//            current_DSP_program_control_port_ = data;
+//            if (current_DSP_program_control_port_&DSP_T0) {
+//                // D0 Bus DMA execution
+//
+//            }
+//            if (current_DSP_program_control_port_&DSP_EX) {
+//#ifdef _DEBUGEMU
+//                std::ostringstream oss;
+//                oss << "DSP program execution started at address : " << showbase << hex << static_cast<uint32_t>((current_DSP_program_control_port_ & DSP_P));
+//                EmuState::pLog->ScuDsp(oss.str());
+//#endif
+//                PC_ = static_cast<uint8_t>((current_DSP_program_control_port_ & DSP_P));
+//                DSPProgramDisasm(PC_);
+//                current_DSP_program_control_port_ &= ~DSP_EX; // DSP interception
+//                SetISTFlag(0x45); // DSP interrupt flag
+//            }
+//            break;
+//        case dsp_program_ram_dataport:
+//            index = static_cast<uint8_t>((current_DSP_program_control_port_ & DSP_P));
+//
+//            RawWrite32(pProgramRam_, index, data);
+//            index++;
+//            current_DSP_program_control_port_ &= 0xFFFFFF00;
+//            current_DSP_program_control_port_ |= static_cast<uint8_t>(index);
+//
+//            current_DSP_program_dataport_ = data;
+//            break;
+//        case dsp_data_ram_address_port:
+//            current_DSP_data_ram_address_port_ = static_cast<uint8_t>(data);
+//            break;
+//        case dsp_data_ram_data_port:
+//            current_DSP_data_ram_dataport_ = data;
+//            break;
+//        case level_0_dma_enable_register:
+//        case level_1_dma_enable_register:
+//        case level_2_dma_enable_register:
+//        case dma_status_register + 0x0C:
+//            // DMA registers write
+//            dmaUpdate_ = true;
+//            break;
+//    }
+
+    //if(address==0x25FE00A0) DebugBreak();
+    rawWrite<u32>(memory_->scu_, addr & 0xFF, data);
+};
+
 void Scu::initializeRegisters() {
     // DMA
-    rawWrite<u32>(memory_->scu_, (level_0_dma_set_register + 0x0C) & 0xFF, 0x00000101);
-    rawWrite<u32>(memory_->scu_, (level_0_dma_set_register + 0x10) & 0xFF, 0x00000000);
-    rawWrite<u32>(memory_->scu_, (level_0_dma_set_register + 0x14) & 0xFF, 0x00000007);
-    rawWrite<u32>(memory_->scu_, (level_1_dma_set_register + 0x2C) & 0xFF, 0x00000101);
-    rawWrite<u32>(memory_->scu_, (level_1_dma_set_register + 0x30) & 0xFF, 0x00000000);
-    rawWrite<u32>(memory_->scu_, (level_1_dma_set_register + 0x34) & 0xFF, 0x00000007);
-    rawWrite<u32>(memory_->scu_, (level_2_dma_set_register + 0x4C) & 0xFF, 0x00000101);
-    rawWrite<u32>(memory_->scu_, (level_2_dma_set_register + 0x50) & 0xFF, 0x00000000);
-    rawWrite<u32>(memory_->scu_, (level_2_dma_set_register + 0x54) & 0xFF, 0x00000007);
-    rawWrite<u32>(memory_->scu_, dma_forced_stop & 0xFF, 0x00000000);
-    rawWrite<u32>(memory_->scu_, (dma_status_register + 0x0C) & 0xFF, 0x00000000);
+    rawWrite<u32>(memory_->scu_, level_0_dma_add_value_register & 0xFF, 0x00000101);
+    rawWrite<u32>(memory_->scu_, level_0_dma_enable_register & 0xFF,    0x00000000);
+    rawWrite<u32>(memory_->scu_, level_0_dma_mode_register & 0xFF,      0x00000007);
+    rawWrite<u32>(memory_->scu_, level_1_dma_add_value_register & 0xFF, 0x00000101);
+    rawWrite<u32>(memory_->scu_, level_1_dma_enable_register & 0xFF,    0x00000000);
+    rawWrite<u32>(memory_->scu_, level_1_dma_mode_register & 0xFF,      0x00000007);
+    rawWrite<u32>(memory_->scu_, level_2_dma_add_value_register & 0xFF, 0x00000101);
+    rawWrite<u32>(memory_->scu_, level_2_dma_enable_register & 0xFF,    0x00000000);
+    rawWrite<u32>(memory_->scu_, level_2_dma_mode_register & 0xFF,      0x00000007);
+    rawWrite<u32>(memory_->scu_, dma_forced_stop & 0xFF,                0x00000000);
+    rawWrite<u32>(memory_->scu_, (dma_status_register + 0x0C) & 0xFF,   0x00000000);
     
     // DSP
     rawWrite<u32>(memory_->scu_, dsp_program_control_port & 0xFF,  0x00000000);
