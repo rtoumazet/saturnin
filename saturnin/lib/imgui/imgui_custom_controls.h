1fb0fcf407b0015556364bf99434862d9f2365ad
Runik <rtoumazet@free.fr>
2019-10-08 22:30:53 +0200

    Added imgui_custom_controls

---
+++ saturnin/lib/imgui/imgui_custom_controls.h
@@ -0,0 +1,55 @@
+// 
+// imgui_custom_controls.h
+// Saturnin
+//
+// Copyright (c) 2019 Renaud Toumazet
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// \file	imgui_custom_controls.h
+///
+/// \brief	Regroups various wrappers for imgui
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <imgui.h>
+
+namespace ImGui {
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// \fn bool ImageButtonWithText(ImTextureID texId, const char* label, const ImVec2& imageSize = ImVec2(0, 0), const ImVec2& uv0 = ImVec2(0, 0), 
+    ///     const ImVec2& uv1 = ImVec2(1, 1), int frame_padding = -1, const ImVec4& bg_col = ImVec4(0, 0, 0, 0), const ImVec4& tint_col = ImVec4(1, 1, 1, 1));
+    ///
+    /// \brief  Image button with text.
+    ///
+    /// \author Runik
+    /// \date   07/10/2019
+    ///
+    /// \param  texId           Texture identifier.
+    /// \param  label           Button label.
+    /// \param  imageSize       (Optional) Size of the image.
+    /// \param  uv0             (Optional) uv0 coordinates.
+    /// \param  uv1             (Optional) uv1 coordinates.
+    /// \param  frame_padding   (Optional) The frame padding.
+    /// \param  bg_col          (Optional) The background col.
+    /// \param  tint_col        (Optional) The tint col.
+    ///
+    /// \return True if it succeeds, false if it fails.
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    bool ImageButtonWithText(ImTextureID texId, const char* label, const ImVec2& imageSize = ImVec2(0, 0), const ImVec2& uv0 = ImVec2(0, 0),
+        const ImVec2& uv1 = ImVec2(1, 1), int frame_padding = -1, const ImVec4& bg_col = ImVec4(0, 0, 0, 0), const ImVec4& tint_col = ImVec4(1, 1, 1, 1));
+    
+}
